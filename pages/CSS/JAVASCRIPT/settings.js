2d435e46a607c40b6bc4fa2dd5c528ea47e656da
Sandhya <ragirisandhya24@gmail.com>
2026-06-04 20:32:19 +0530

    practied Siblings

--- pages/CSS/JAVASCRIPT/settings.js
+++ pages/CSS/JAVASCRIPT/settings.js
@@ -1,7 +1,8 @@
 const settings = document.querySelector(".settings");
 const modal = document.querySelector(".settings-modal");
 const closeBtn = document.querySelector(".close-btn");
+const Mybutton = document.querySelector(".myButton");
+const para = document.querySelector("#paraText");
 
-closeBtn.addEventListener("click", () => {
-    modal.classList.add("hidden");
-});
+
+
