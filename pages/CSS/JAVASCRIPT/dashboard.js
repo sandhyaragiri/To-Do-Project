e83c65b72fac50dd5bef4893742d7c66cab44af4
Sandhya <ragirisandhya24@gmail.com>
2026-06-04 13:52:44 +0530

    modified settings div

--- pages/CSS/JAVASCRIPT/dashboard.js
+++ pages/CSS/JAVASCRIPT/dashboard.js
@@ -9,3 +9,15 @@ const settings = document.querySelector("#settings");
 settings.addEventListener("click", () => {
    window.location.href = "./pages/settings.html";
 });
+
+const timer = document.querySelector("#timer");
+
+timer.addEventListener("click", ()=>{
+    alert("WORK IN PROGRESS");
+});
+
+const notes = document.querySelector("#notes");
+
+notes.addEventListener("click", ()=>{
+    alert("WORK IN PROGRESS");
+});
--- pages/CSS/JAVASCRIPT/settings.js
+++ pages/CSS/JAVASCRIPT/settings.js
@@ -2,7 +2,6 @@ const settings = document.querySelector(".settings");
 const modal = document.querySelector(".settings-modal");
 const closeBtn = document.querySelector(".close-btn");
 
-
 closeBtn.addEventListener("click", () => {
     modal.classList.add("hidden");
 });
