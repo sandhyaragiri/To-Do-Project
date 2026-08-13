9da422eb1d1d3743065ca9b66bac536b1231b6f5
Sandhya <ragirisandhya24@gmail.com>
2026-08-13 19:27:00 +0530

    working on features

--- pages/CSS/JAVASCRIPT/settings.js
+++ pages/CSS/JAVASCRIPT/settings.js
@@ -2,7 +2,5 @@ const accentSelector = document.querySelector("#accentSelector");
 
 accentSelector.addEventListener("change", function () {
     document.documentElement.style.setProperty(
-        "--accent-color",
-        this.value
-    );
+        "--accent-color",this.value);
 });
